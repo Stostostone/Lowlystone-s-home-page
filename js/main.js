55e6ca7c645a2a5358735d4a42c7b66ed8fd74ad
Stostostone <lowlystone@gmail.com>
2026-06-23 00:13:47 +0800

    add menu page, settings card and javascript functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 function setting() {
     // change the ability of the setting card
     const settingCard = document.querySelector('.setting-card');
-    if (settingCard) {
-        settingCard.style.display = settingCard.style.display === 'none' ? 'block' : 'none';
+
+    if (!settingCard) {
+        return;
+    }
+
+    const isOpen = settingCard.classList.contains("is-open");
+    
+    if (isOpen) {
+        settingCard.classList.remove("is-open");
+        settingCard.classList.add("is-closing");
+
+        settingCard.addEventListener('animationend', () => {
+            settingCard.classList.remove("is-closing");
+        }, { once: true });
+    }
+    else {
+        settingCard.classList.remove("is-closing");
+        settingCard.classList.add("is-open");
     }
 }
+document.querySelector('.setting-button').addEventListener('click', setting);
