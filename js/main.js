9110aedcae96f0bc2faee0dbdc268815c29df5f2
Stostostone <lowlystone@gmail.com>
2026-06-23 00:27:58 +0800

    add page switch function

--- js/main.js
+++ js/main.js
@@ -22,3 +22,29 @@ function setting() {
     }
 }
 document.querySelector('.setting-button').addEventListener('click', setting);
+function showPage(pageClass) {
+    const pages = document.querySelectorAll(".page-section");
+    const targetPage = document.querySelector(pageClass);
+
+    if (!targetPage) {
+        return;
+    }
+
+    pages.forEach((page) => {
+        page.classList.remove("is-active");
+    });
+
+    targetPage.classList.add("is-active");
+}
+
+document.querySelector(".menu-button").addEventListener("click", () => {
+    showPage(".menu-page");
+});
+
+document.querySelector(".tools-button").addEventListener("click", () => {
+    showPage(".tools-page");
+});
+
+document.querySelector(".about-button").addEventListener("click", () => {
+    showPage(".about-page");
+});
